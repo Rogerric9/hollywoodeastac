adca32eb9a4b8a726e4bbbf05608eefe36ce1710
Rogerric9 <steve.kanski@gmail.com>
2026-07-14 13:06:56 -0500

    add shipping

---
+++ shipping.js
@@ -0,0 +1,12 @@
+const SHIPPING_CONFIG = {
+  standardSingle: 6.95,
+  standardMultiple: 8.95,
+
+  framedFirst: 18.95,
+  framedAdditional: 6.00,
+
+  plaqueFirst: 14.95,
+  plaqueAdditional: 4.00,
+
+  domesticOnly: true
+};
